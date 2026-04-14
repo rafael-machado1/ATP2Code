40d98bd68e56b9a0c53edf708c2d23ded3a20297
Rafael <rafaelmachadocd37@gmail.com>
2026-04-14 14:04:31 -0300

    feat: Exercicio pronto.

--- Lista6/src/ex1.c
+++ Lista6/src/ex1.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
-void systemInit();
+int main() 
+{
 
-int main() {
+  int num;
+  int numeros[10] = {0};
+
+  for (int i = 0; i < 10; i++)
+  {
+
+    printf("Digite um número\n");
+    scanf("%d",&numeros[i]);
+
+  }
+
+  printf("[ ");
+
+  for ( int j = 0; j < 10; j++)
+  {
+    
+    printf("%d ",numeros[j]);
+
+  }
+
+  printf(" ]\n");
 
   return 0;
-}
-
-void systemInit() {
-
 }
