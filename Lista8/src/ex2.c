867a7075d919e82fd35818039ef4c4ed52e99134
Rafael <rafaelmachadocd37@gmail.com>
2026-05-19 12:54:04 -0300

    feat: Exercicio pronto.

---
+++ Lista8/src/ex2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    // Variável Composta Heterogênea.
+    // Struct ou Structure
+    /*
+        A struct funciona como uma classe em POO, mas não utiliza de funções.
+        Ela reserva espaço para as variáveis declaradas (fields)
+        Serve como um facilitador e umas ótima maneira de código limpo
+    */ 
+
+    // Segue o exemplo:
+    struct aluno 
+    {
+        int cod;
+        char nome[50]; 
+    };
+
+    return 0;
+
+}
