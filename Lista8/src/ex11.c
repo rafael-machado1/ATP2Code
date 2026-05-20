a3d93918ae5f3160d994bb1c8eca89b1f9b6d331
Rafael <rafaelmachadocd37@gmail.com>
2026-05-20 14:55:45 -0300

    Exercicio pronto.

---
+++ Lista8/src/ex11.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+float media (float n1, float n2)
+{
+    return (n1 + n2) / 2;
+}  
+
+int main()
+{
+
+    // nome, matrícula, nota1, nota2 e média
+
+    typedef struct aluno
+    {
+        int matricula;
+        float nota1;
+        float nota2;
+    } aluno;
+
+    aluno turma[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%f %f", &turma[i].nota1, &turma[i].nota2);
+        turma[i].matricula = i + 1;
+        if (media(turma[i].nota1, turma[i].nota2) >= 6)
+        {
+            printf("Matrícula: %d\nNota 1: %.1f\nNota 2: %.1f\nMedia: %.1f\n ", turma[i].matricula, turma[i].nota1, turma[i].nota2, media(turma[i].nota1, turma[i].nota2));
+        }
+        else
+        {
+            continue;
+        }
+        
+    }
+
+    
+
+    return 0;
+
+}
